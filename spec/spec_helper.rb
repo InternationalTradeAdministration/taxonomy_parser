51543e34a86cc576ef466ce507678d99fef0e467
Tim Hammer <timh@govwizely.com>
2016-01-28 13:38:57 -0500

    Allow code climate requests in test runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'taxonomy_parser'
 
 require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(allow_localhost: true, allow: 'codeclimate.com')
